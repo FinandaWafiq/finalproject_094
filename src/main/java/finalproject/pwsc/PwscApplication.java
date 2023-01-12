b11a467fe4f1715c00ffd13e27e04594c1519669
Finanda Wafiq <finnandawafiq@gmail.com>
2023-01-12 09:38:50 +0700

    Membuat mycontroller pada source packages

---
+++ src/main/java/finalproject/pwsc/PwscApplication.java
@@ -0,0 +1,13 @@
+package finalproject.pwsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwscApplication.class, args);
+	}
+
+}
